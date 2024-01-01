7a3950b2ad085ae8a1fbfbbf30ab2686de4017f1
João Belarmino <joao.belarmino.silva01@gmail.com>
2023-12-31 21:45:04 -0300

    feat: bank accounts crud

---
+++ src/modules/bank-accounts/dto/create-bank-account.dto.ts
@@ -0,0 +1,21 @@
+import { IsEnum, IsHexadecimal, IsHexColor, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { BankAccountType } from '../entities/BankAccount';
+
+export class CreateBankAccountDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  initialBalance: number;
+
+  @IsNotEmpty()
+  @IsEnum(BankAccountType)
+  type: BankAccountType;
+
+  @IsString()
+  @IsHexColor()
+  @IsNotEmpty()
+  color: string;
+}
---
+++ src/modules/bank-accounts/services/bank-accounts.service.ts
@@ -0,0 +1,67 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateBankAccountDto } from '../dto/create-bank-account.dto';
+import { UpdateBankAccountDto } from '../dto/update-bank-account.dto';
+import { BankAccountsRepository } from '../../../shared/database/repositories/bank-accounts.repositories';
+import { ValidateBankAccountOwnershipService } from './validate-bank-account-ownership.service';
+
+@Injectable()
+export class BankAccountsService {
+  constructor(
+    private readonly bankAccountsRepo: BankAccountsRepository,
+    private readonly validateBankAccountOwnershipService: ValidateBankAccountOwnershipService,
+  ) {}
+
+  create(userId: string, createBankAccountDto: CreateBankAccountDto) {
+    const { name, initialBalance, type, color } = createBankAccountDto;
+
+    return this.bankAccountsRepo.create({
+      data: {
+        userId,
+        name,
+        initialBalance,
+        type,
+        color,
+      },
+    });
+  }
+
+  findAllByUserId(userId: string) {
+    return this.bankAccountsRepo.findMany({ where: { userId } });
+  }
+
+  async update(
+    userId: string,
+    bankAccountId: string,
+    updateBankAccountDto: UpdateBankAccountDto,
+  ) {
+    await this.validateBankAccountOwnershipService.validate(
+      userId,
+      bankAccountId,
+    );
+
+    const { name, initialBalance, type, color } = updateBankAccountDto;
+
+    return this.bankAccountsRepo.update({
+      data: {
+        name,
+        initialBalance,
+        type,
+        color,
+      },
+      where: { id: bankAccountId },
+    });
+  }
+
+  async remove(userId: string, bankAccountId: string) {
+    await this.validateBankAccountOwnershipService.validate(
+      userId,
+      bankAccountId,
+    );
+
+    await this.bankAccountsRepo.delete({
+      where: { id: bankAccountId },
+    });
+
+    return null;
+  }
+}
